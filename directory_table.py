981b7d9ac8d959135f0eab196194d67a4e96bccc
Joshua Shrestha <33375383+joshrestha7@users.noreply.github.com>
2018-10-13 06:32:14 -0400

    linked all pages to flask :)

--- directory_table.py
+++ directory_table.py
@@ -10,6 +10,22 @@ def landing():
 def login():
     return render_template("log_in.html")
 
+@app.route('/learn')
+def learn():
+    return render_template("free_learning_center.html")
+
+@app.route('/lesson_1')
+def lesson_1():
+    return render_template("lesson_1.html")
+
+@app.route('/premium')
+def premium():
+    return render_template("premium.html")
+
+@app.route('/home')
+def home_page():
+	return render_template("home_page.html")
+
 @app.route('/social')
 def feed():
     return render_template("social_media.html")
